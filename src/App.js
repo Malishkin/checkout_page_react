d981efe9847a788f16ca8738c65358cdd4a7e738
Malishkin <malishkin2002@gmail.com>
2022-03-29 10:34:00 +0200

    every component remain responsive

--- src/App.js
+++ src/App.js
@@ -6,16 +6,27 @@ import WhatContainer from './components/what-container/WhatContainer';
 import SubTotalContainer from './components/subtotal-container/SubTotalContainer';
 import PreferContainer from './components/prefer-container/PreferContainer';
 import ProcessOrderYes from './components/process-order-yes-container/ProcessOrderYes';
+import FooterComp from './components/footer/FooterComp';
+import ReasonsContainer from './components/reasons-container/ReasonsContainer';
+import PaymentInfoContainer from './components/payment-info-container/PaymentInfoContainer';
+import ShippingContainer from './components/shipping-container/ShippingContainer';
+import BillingContainer from './components/billing-container/BillingContainer';
+
 function App() {
   return (
     <div>
-      <Navbar />
+      <Navbar/>
       <Header />
       <TopContainer />
       <WhatContainer />
       <SubTotalContainer/>
       <PreferContainer/>
+      <BillingContainer/>
+      <ShippingContainer/>
+      <PaymentInfoContainer/>
       <ProcessOrderYes/>
+      <ReasonsContainer/>
+      <FooterComp/>
     </div>
   );
 }
